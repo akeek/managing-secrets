0095878ed8eb698d43a56aae113e2051953ec9fc
Åke Ek <svein.ek@stud.noroff.no>
2022-10-26 15:47:49 +0200

    Updates

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 require("dotenv/config");
 
 function test(expectedSecret) {
-    const secretMatches = my.env.MEANING_OF_LIFE == expectedSecret;
-    console.log(`The meaning of life is ${my.env.MEANING_OF_LIFE}`);
+    const secretMatches = process.env.MEANING_OF_LIFE == expectedSecret;
+    console.log(`The meaning of life is ${process.env.MEANING_OF_LIFE}`);
     console.log(`The secret does${secretMatches ? "" : " not"} match!`);
     return secretMatches;
   }
